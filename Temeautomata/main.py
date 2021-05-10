b4584a246def2e85ced9ff88029adfaec9b1525d
bboybogdan <63145948+bboybogdan@users.noreply.github.com>
2021-05-10 09:36:21 +0300

    Add files via upload

---
+++ Temeautomata/main.py
@@ -0,0 +1,16 @@
+import wikipedia
+from docx import Document
+
+language = "ro"
+wikipedia.set_lang(language)
+document = Document()
+
+x = input("Despre ce subiect este tema? ")
+
+titlu = wikipedia.page(x).title
+continut = wikipedia.page(x).content
+document.add_heading(titlu)
+paragraph = document.add_paragraph(continut)
+content = titlu + "\n" + continut
+
+document.save('tema.docx')
