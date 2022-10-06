d9ea1ce432f9c124b2f5ab9fcca0b4e6b89001ee
Martin Bogdan <63145948+bboybogdan@users.noreply.github.com>
2022-10-06 07:28:53 +0300

    Add files via upload

--- Python/spam.py
+++ Python/spam.py
@@ -5,5 +5,5 @@ x = int(input("Cate mesaje vrei sa trimiti? "))
 keyboard.wait('space')
 
 for i in range(x):
-    keyboard.write('SPAM')
+    keyboard.write('Vrei o bere?')
     keyboard.press_and_release('enter')
